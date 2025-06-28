4cb9d668cc088e7dbcd85e1de32b51e01679d213
Otto Werse <otto.werse@web.de>
2025-06-28 22:01:37 +0200

    Added overwrite option since download output is incomplete directory anyway.

--- zahhak.py
+++ zahhak.py
@@ -1457,6 +1457,7 @@ def download_video(video):
             'no_warnings': quiet_download_warnings,
             'download_archive': None,  # TODO: This is correct ,yes?
             'cachedir': False,
+            'overwrites': True,
             'skip_unavailable_fragments': False,  # To abort on missing video parts (largely avoids re-downloading)
             'ignoreerrors': False,
             'ignore_no_formats_error': False,
