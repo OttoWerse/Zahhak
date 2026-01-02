86e3f67a85fe7119a62e33066ec20e745a699842
Otto Werse <olwerse@gmail.com>
2026-01-02 02:54:43 +0100

    - Changed format slightly, first to include different spellings of VP9 (vp9/vp09) and AV1 (av01/av1) and second to check width <= 2000 instead of looking at height. Work smart, not hard.

--- zahhak.py
+++ zahhak.py
@@ -31,7 +31,7 @@ mysql_user = os.getenv('ZAHHAK_MYSQL_USERNAME', 'admin')
 mysql_password = os.getenv('ZAHHAK_MYSQL_PASSWORD', 'admin')
 
 '''Variables'''
-MEDIA_FORMAT = "bestvideo*[ext=mp4][height<=1200][vcodec~='^(av01|vp9|h265|hevc)']+bestaudio[ext=m4a]"
+MEDIA_FORMAT = "bestvideo*[ext=mp4][width<=2000][vcodec~='^(av0?1|vp0?9|h265|hevc)']+bestaudio[ext=m4a]"
 # Frequency to reconnect VPN (in seconds)
 sleep_time_vpn = 10
 # How often to retry connecting to a VPN country before giving up
